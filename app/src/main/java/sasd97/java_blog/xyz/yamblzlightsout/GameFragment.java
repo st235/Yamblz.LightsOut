35265f79486770d8c63262bbcdec1add0fdd4608
Alena Malchikhina <alyonamalchikhina@gmail.com>
2017-08-12 17:57:07 +0300

    timer added

--- app/src/main/java/sasd97/java_blog/xyz/yamblzlightsout/GameFragment.java
+++ app/src/main/java/sasd97/java_blog/xyz/yamblzlightsout/GameFragment.java
@@ -1,12 +1,15 @@
 package sasd97.java_blog.xyz.yamblzlightsout;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import sasd97.java_blog.xyz.yamblzlightsout.ui.GameView;
 import xyz.javablog.Engine;
@@ -22,19 +25,75 @@ import xyz.javablog.common.sizes.SquareSize;
 public class GameFragment extends Fragment {
 
     private GameView gameView;
+    private FrameLayout frameLayout;
+    private TextView textView;
+    private long startTime;
+    private Handler timerHandler = new Handler();
+    private Runnable timerRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+            textView.setText(setTime(millis / 1000));
+            timerHandler.postDelayed(this, 1000);
+        }
+    };
+
+    private String setTime(long time) {
+            String timeHHMMS = "00:00";
+            int sec = 0;
+            int min = 0;
+            int hour;
+            String s;
+            String m;
+
+            sec = (int) time % 60;
+            hour = (int) (time / 3600);
+            min = (int) (time % 3600) / 60;
+
+            if (hour == 0) {
+
+                if (sec < 10) {
+                    s = "0" + sec;
+                } else s = String.valueOf(sec);
+                if (min < 10) {
+
+                    m = "0" + min;
+                } else m = String.valueOf(min);
+
+                timeHHMMS = m + ":" + s;
+
+            } else {
+
+                if (sec < 10) {
+                    s = "0" + sec;
+                } else s = String.valueOf(sec);
+                if (min < 10) {
+
+                    m = "0" + min;
+                } else m = String.valueOf(min);
+
+                timeHHMMS = hour + ":" + m + ":" + s;
+            }
+            return timeHHMMS;
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_game, container, false);
 
+        startTime = System.currentTimeMillis();
+        frameLayout = v.findViewById(R.id.containerFragment);
+        textView = v.findViewById(R.id.timer_tv);
         final Engine engine = GameEngineProvider.getEngine();
         Field field = engine.obtainNewField(new SquareSize(5));
         Log.d("HERE", field.toString());
+        timerHandler.postDelayed(timerRunnable, 0);
 
         gameView = v.findViewById(R.id.fragment_game_game_view);
         gameView.setNewSize(new SquareSize(5));
-
+        gameView.measure(frameLayout.getWidth(), frameLayout.getHeight());
         gameView.setMatrix(field.getMatrix());
 
         gameView.setListener(new GameView.OnGridItemClickListener() {
@@ -44,7 +103,7 @@ public class GameFragment extends Fragment {
                 Field f = engine.clickCurrentField(new Click(x, y));
                 Log.d("HERE", f.toString());
                 gameView.setMatrix(f.getMatrix());
-                gameView.invalidate();
+
             }
         });
 

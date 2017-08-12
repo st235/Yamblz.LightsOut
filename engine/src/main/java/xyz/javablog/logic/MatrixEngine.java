34b79375e61c97b93206f9dcd4acd6354991664b
alexander <st235@yandex.ru>
2017-08-12 12:05:41 +0300

    Implemented matrix engine

--- engine/src/main/java/xyz/javablog/logic/MatrixEngine.java
+++ engine/src/main/java/xyz/javablog/logic/MatrixEngine.java
@@ -1,9 +1,10 @@
 package xyz.javablog.logic;
 
 import xyz.javablog.models.Field;
+import xyz.javablog.models.Matrix;
 import xyz.javablog.models.Point;
 
 public interface MatrixEngine {
-    Field pushForward(Point point);
-    Field pushBackward(Point point);
+    Field pushForward(Point point, Matrix matrix);
+    Field pushBackward(Point point, Matrix matrix);
 }

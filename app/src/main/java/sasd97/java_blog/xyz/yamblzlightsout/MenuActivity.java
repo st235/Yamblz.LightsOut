cac520f58eb7ae799b375200b1e350a37da6fdcb
alexander <st235@yandex.ru>
2017-08-12 16:18:05 +0300

    Fixed ui

---
+++ app/src/main/java/sasd97/java_blog/xyz/yamblzlightsout/MenuActivity.java
@@ -0,0 +1,15 @@
+package sasd97.java_blog.xyz.yamblzlightsout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    private static final String TAG = MenuActivity.class.getCanonicalName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
